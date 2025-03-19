4b94856746d295b3e082091c3d879a763916f20d
Veronika Sheluhina <lartyera.maresa@mail.ru>
2025-03-19 21:16:02 +0400

    the program calculates the n-th fibonacci number using the recursive function fib(n), which returns 1 at values n=1 and n=2, and at subsequent values of n returns the sum of the n-1st and n-2nd values; then the number n is entered into main and the result is printed as the n-th fibonacci number.

--- main1.cpp
+++ main1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-cout << "Hello, world!";
-return 0;
+int fib(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else if (n == 2) {
+        return 1;
+    }
+    else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+    return 0;
 }
